960a99aaf22083476413c1022ef53fad405cf547
moritzrfs <moritz.rfs@gmail.com>
2022-05-24 15:33:58 +0200

    added wall class

--- Objekte-Plugin/src/main/java/entities/Wall.java
+++ Objekte-Plugin/src/main/java/entities/Wall.java
@@ -1,4 +1,14 @@
 package entities;
 
-public class Wall {
+public class Wall extends StaticEntity{
+
+    private int length;
+    private int height;
+    private int xPos;
+    private int yPos;
+    private World world;
+    private Plugin plugin;
+    
+    public Wall(){        
+    }
 }
