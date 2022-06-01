2d1bf7066ed8e703f7e7448a5f8be68e239b7ed5
NMD03 <nicjan01@gmail.com>
2022-06-01 13:58:28 +0200

    added messages

--- Objekte-Plugin/src/main/java/entities/Door.java
+++ Objekte-Plugin/src/main/java/entities/Door.java
@@ -3,25 +3,26 @@ package entities;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.World;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.StaticEntity;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.simulation.SimulationWorld;
 import lombok.Data;
 
-public class Door extends StaticEntity {
+abstract class Door extends StaticEntity {
     private int length;
     private int height;
-    private int xPos;
-    private int yPos;
-    private World world;
+    protected int xPos;
+    protected int yPos;
+    private SimulationWorld world;
     private Plugin plugin;
     private boolean isOpen;
     private int startLength;
     private int startHeigth;
 
-    public Door(int length, int height, int xPos, int yPos, World world, Plugin plugin, boolean isOpen){
+    public Door(int length, int height, int xPos, int yPos, SimulationWorld world, Plugin plugin, boolean isOpen){
         this.length = length;
         this.height = height;
         this.xPos = xPos;
         this.yPos = yPos;
-        this. world = world;
+        this.world = world;
         this.plugin = plugin;
         this.isOpen = isOpen;
         this.startHeigth = height;
--- Objekte-Plugin/src/main/java/entities/TicketCounter.java
+++ Objekte-Plugin/src/main/java/entities/TicketCounter.java
@@ -1,8 +1,13 @@
 package entities;
 
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.geometry.Point;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.World;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.StaticEntity;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.Entity;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.message.Message;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.message.DirectedMessage;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.message.LocalMessage;
 
 public class TicketCounter extends Door {
     public TicketCounter(int length, int height, int xPos, int yPos, World world, Plugin plugin, boolean isOpen){
@@ -12,4 +17,35 @@ public class TicketCounter extends Door {
     public boolean checkTicket(){
         return true;
     }
+
+    public void pluginUpdate(){
+        getWorld().sendMessage(new TicketCheck(this.xPos, this.yPos));
+    }
+
+    public void receiveMessage(Message m){
+        if(m instanceof TicketCheck){
+            this.openDoor();
+        }
+    }
+
+    private static class TicketCheck implements LocalMessage {
+        int xPos;
+        int yPos;
+        public TicketCheck(int xPos, int yPos){
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+        public Entity getOrigin(){
+            return null;
+        }
+
+        public Point getOriginPosition(){
+            return new Point(this.xPos, this.yPos);
+        }
+
+        public int getMaxRange(){
+            return 1;
+        }
+    }
+
 }
