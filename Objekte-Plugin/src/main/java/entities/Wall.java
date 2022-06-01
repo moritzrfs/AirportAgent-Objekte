38347e7f49a764b0e9503a5779618f9ced9b8988
NMD03 <nicjan01@gmail.com>
2022-06-01 14:20:51 +0200

    added constructor

--- Objekte-Plugin/src/main/java/entities/Wall.java
+++ Objekte-Plugin/src/main/java/entities/Wall.java
@@ -3,6 +3,7 @@ package entities;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.World;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.StaticEntity;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.simulation.SimulationWorld;
 import lombok.Data;
 
 @Data
@@ -12,10 +13,16 @@ public class Wall extends StaticEntity {
     private int height;
     private int xPos;
     private int yPos;
-    private World world;
+    private SimulationWorld world;
     private Plugin plugin;
 
-    public Wall(){
+    public Wall(int length, int height, int xPos, int yPos, SimulationWorld world, Plugin plugin) {
+        this.length = length;
+        this.height = height;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.world = world;
+        this.plugin = plugin;
     }
 
     @Override
