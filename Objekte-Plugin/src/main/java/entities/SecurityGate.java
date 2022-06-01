96afb04985734c580b2de0785d1f0f1f17dcab53
NMD03 <nicjan01@gmail.com>
2022-06-01 14:02:20 +0200

    added simulationWorld

--- Objekte-Plugin/src/main/java/entities/SecurityGate.java
+++ Objekte-Plugin/src/main/java/entities/SecurityGate.java
@@ -3,10 +3,11 @@ package entities;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.World;
 import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.simulation.entity.StaticEntity;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.simulation.SimulationWorld;
 
 public class SecurityGate extends Door {
 
-    public SecurityGate(int length, int height, int xPos, int yPos, World world, Plugin plugin, boolean isOpen){
+    public SecurityGate(int length, int height, int xPos, int yPos, SimulationWorld world, Plugin plugin, boolean isOpen){
         super(length, height, xPos, yPos, world, plugin, isOpen);
     }
 
