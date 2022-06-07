35ac59242c12786c3f970dc20b8e89a2ed5cc7aa
moritzrfs <moritz.rfs@gmail.com>
2022-06-07 11:26:02 +0200

    add entity registrations in activate method

--- Objekte-Plugin/src/main/java/plugin/ObjectPlugin.java
+++ Objekte-Plugin/src/main/java/plugin/ObjectPlugin.java
@@ -1,9 +1,21 @@
 package plugin;
 
+import entities.BaggageDropOff;
+import entities.*;
+
 public class ObjectPlugin implements dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin {
 
     @Override
     public void activate() {
+        try {
+            AirportAgentSimulation.registerEntity(this, "wall", Wall.class);
+            AirportAgentSimulation.registerEntity(this, "baggageDropOff", BaggageDropOff.class);
+            AirportAgentSimulation.registerEntity(this, "door", Door.class);
+            AirportAgentSimulation.registerEntity(this, "securityGate", SecurityGate.class);
+            AirportAgentSimulation.registerEntity(this, "ticketCounter", TicketCounter.class);
+        } catch (ConfigurationFormatException e) {
+            e.printStackTrace();
+        }
 
     }
 }
